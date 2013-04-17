7b6dddd18a3ba4deebb14afa1efa21ae0390b5e8
cpldcpu <cpldcpu@gmail.com>
2013-04-17 10:06:30 +0200

    Initial Project

---
+++ light_ws2812/light_ws2812.c
@@ -0,0 +1,17 @@
+/*
+ * light_ws2812.c
+ *
+ * Created: 17.04.2013 10:01:20
+ *  Author: Tim
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
