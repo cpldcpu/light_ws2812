b3e3a27cebd94cd9fdd6f6aa07d01503bb13c9a4
cpldcpu <cpldcpu@gmail.com>
2013-08-30 08:27:50 +0200

    new example added

---
+++ light_ws2812_AVR/Example_chained_writes.c
@@ -0,0 +1,58 @@
+/*
+ * Light_WS2812 library example - Chained Writes
+ *
+ * This example shows how to use multiple calls without issuing a reset 
+ * in between, allowing to send the same buffer to the string
+ * multiple times. This technique can be useful to conserve memory
+ * or to calculate LED colors on-the-fly.
+ *
+ * This example is configured for a ATtiny85 with PLL clock fuse set and 
+ * the WS2812 string connected to PB4. 
+ */ 
+
+#define F_CPU 16000000
+
+#include <util/delay.h>
+#include <avr/io.h>
+#include "light_ws2812.h"
+
+struct CRGB { uint8_t g; uint8_t r; uint8_t b; };
+struct CRGB led[2];
+
+int main(void)
+{
+	uint8_t mask;
+	uint8_t pos=0;
+	uint8_t direction=1;	
+	uint8_t i;
+	
+	#ifdef __AVR_ATtiny10__
+		CCP=0xD8;		// configuration change protection, write signature
+		CLKPSR=1;		// set cpu clock prescaler =1 (8Mhz) (attiny 4/5/9/10)
+		mask=_BV(PB2);
+	#else
+		CLKPR=_BV(CLKPCE);
+		CLKPR=0;			// set clock prescaler to 1 (attiny 25/45/85/24/44/84/13/13A)		
+		mask=_BV(PB4);
+	#endif
+		DDRB|=mask;
+
+	led[0].r=16;led[0].g=00;led[0].b=00;		// LED 0 is red
+	led[1].r=16;led[1].g=16;led[1].b=16;		// LED 1 is White
+	
+	while(1)
+    {
+	
+		for (i=0; i<pos; i++) 
+			ws2812_sendarray((uint8_t *)&led[0],3);			// Repeatedly send "red" to the led string. 
+															// No more than 1-2µs should pass between calls
+															// to avoid issuing a reset condition.			
+		for (i=0; i<(16-pos); i++) 
+			ws2812_sendarray((uint8_t *)&led[1],3);			// white
+		
+		_delay_ms(50);										// Issue reset and wait for 50 ms.
+		
+		pos+=direction;		
+		if ((pos==16)||(pos==0)) direction=-direction;
+    }
+}
