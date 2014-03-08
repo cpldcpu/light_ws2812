36210edef62c58fe8e41066c8c564f287e6de505
cpldcpu <cpldcpu@gmail.com>
2014-03-08 18:48:48 +0100

    arduino: rename example to "blinky"

--- light_ws2812_Arduino/light_WS2812/examples/Blinky/Blinky.ino
+++ light_ws2812_Arduino/light_WS2812/examples/Blinky/Blinky.ino
@@ -7,7 +7,6 @@
 
 #include <WS2812.h>
 
-
 WS2812 LED(1); // 1 LED
 	
 cRGB value;
@@ -20,11 +19,11 @@ void loop() {
 	value.b = 255; value.g = 0; value.r = 0; // RGB Value -> Blue
 	LED.set_crgb_at(0, value); // Set value at LED found at index 0
 	LED.sync(); // Sends the value to the LED
-	delay(50); // Wait 50 ms
+	delay(500); // Wait 500 ms
 	
 	value.b = 0; value.g = 0; value.r = 255; // RGB Value -> Red
 	LED.set_crgb_at(0, value); // Set value at LED found at index 0
 	LED.sync(); // Sends the value to the LED
-	delay(50); // Wait 50 ms
+	delay(500); // Wait 500 ms
 }
 
