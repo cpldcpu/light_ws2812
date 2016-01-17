cac1b53765182263ceb0fe3366f1936016cae6a8
cpldcpu <cpldcpu@gmail.com>
2016-01-18 00:01:57 +0100

    fix example rainbow.c

--- light_ws2812_AVR/Examples/Rainbow.c
+++ light_ws2812_AVR/Examples/Rainbow.c
@@ -8,9 +8,9 @@
 #include <avr/interrupt.h>
 #include "light_ws2812.h"
 
-#define MAXPIX 253
-#define COLORLENGTH 100
-#define FADE 5
+#define MAXPIX 30
+#define COLORLENGTH MAXPIX/2
+#define FADE 256/COLORLENGTH
 
 struct cRGB colors[8];
 struct cRGB led[MAXPIX];
@@ -49,45 +49,36 @@ int main(void)
         if(k>COLORLENGTH)
         {
             j++;
+            if(j>7)
+            {
+              j=0;
+            }
+
             k=0;
         }
         k++;
         //loop colouers
-        if(j>8)
-            j=0;
         
         //fade red
-        if(led[0].r<colors[j].r)
+        if(led[0].r<(colors[j].r-FADE))
             led[0].r+=FADE;
-        if(led[0].r>255.r)
-            led[0].r=255;
             
-        if(led[0].r>colors[j].r)
+        if(led[0].r>(colors[j].r+FADE))
             led[0].r-=FADE;
-    //    if(led[0].r<0)
-    //        led[0].r=0;
-        //fade green
-        if(led[0].g<colors[j].g)
-            led[0].g+=FADE;
-        if(led[0].g>255)
-            led[0].g=255;
-            
-        if(led[0].g>colors[j].g)
-            led[0].g-=FADE;
-    //    if(led[0].g<0)
-    //        led[0].g=0;
-        //fade blue
-        if(led[0].b<colors[j].b)
-            led[0].b+=FADE;
-        if(led[0].b>255)
-            led[0].b=255;
-            
-        if(led[0].b>colors[j].b)
-            led[0].b-=FADE;
-    //    if(led[0].b<0)
-    //        led[0].b=0;
 
-		 _delay_ms(10);										
+        if(led[0].g<(colors[j].g-FADE))
+            led[0].g+=FADE;
+            
+        if(led[0].g>(colors[j].g+FADE))
+            led[0].g-=FADE;
+
+        if(led[0].b<(colors[j].b-FADE))
+            led[0].b+=FADE;
+            
+        if(led[0].b>(colors[j].b+FADE))
+            led[0].b-=FADE;
+
+		 _delay_ms(10);
 		 ws2812_sendarray((uint8_t *)led,MAXPIX*3);
     }
 }
