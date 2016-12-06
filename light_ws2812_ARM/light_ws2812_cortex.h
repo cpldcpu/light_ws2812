e47399c5ea84832a90b78b63b91c2fee53555a78
Oliv4945 <oliv4945@gmail.com>
2016-12-06 20:55:31 +0100

    Add plateform selection and STM32 Use only defines to change target

--- light_ws2812_ARM/light_ws2812_cortex.h
+++ light_ws2812_ARM/light_ws2812_cortex.h
@@ -9,11 +9,23 @@
 #define LIGHT_WS2812_H_
 
 ///////////////////////////////////////////////////////////////////////
+// User defined area: Select CPU type
+///////////////////////////////////////////////////////////////////////
+// #define LIGHT_WS2812_LPC8XX
+#define LIGHT_WS2812_STM32L0XX
+
+///////////////////////////////////////////////////////////////////////
 // User defined area: CPU specific CMSIS include
 ///////////////////////////////////////////////////////////////////////
 
-#include "LPC8XX.h"			// Change this to the include for your MCU
-
+#ifdef LIGHT_WS2812_LPC8XX
+  #include "LPC8XX.h"			// Change this to the include for your MCU
+  #define LIGHT_WS2812_LPC
+#endif
+#ifdef LIGHT_WS2812_STM32L0XX
+  #include "stm32l0xx_hal.h"
+  #define LIGHT_WS2812_STM32
+#endif
 
 ///////////////////////////////////////////////////////////////////////
 // User defined area: Define I/O pin
@@ -21,15 +33,23 @@
 
 // Data port and pin definition for your CPU. Depending on the way
 // port access is implemented in your Cortex CPU, the set and clr
-// mask and addresses may be the same. This example is for the
-// NXP LPC81X
+// mask and addresses may be the same.
+#ifdef LIGHT_WS2812_LPC
+  // This example is for the NXP LPC81X
+  #define ws2812_port_set ((uint32_t*)&LPC_GPIO_PORT->SET0)	// Address of the data port register to set the pin
+  #define ws2812_port_clr	((uint32_t*)&LPC_GPIO_PORT->CLR0)	// Address of the data port register to clear the pin
 
-#define ws2812_port_set ((uint32_t*)&LPC_GPIO_PORT->SET0)	// Address of the data port register to set the pin
-#define ws2812_port_clr	((uint32_t*)&LPC_GPIO_PORT->CLR0)	// Address of the data port register to clear the pin
+  #define ws2812_mask_set  (1<<2)		// Bitmask to set the data out pin
+  #define ws2812_mask_clr  (1<<2)		// Bitmask to clear the data out pin
+#endif
+#ifdef LIGHT_WS2812_STM32
+  // This example is for STM32 family
+  #define ws2812_port_set ((uint32_t*)&GPIOA->BSRR)  // Address of the data port register to set the pin
+  #define ws2812_port_clr ((uint32_t*)&GPIOA->BRR) // Address of the data port register to clear the pin
 
-#define ws2812_mask_set  (1<<2)		// Bitmask to set the data out pin
-#define ws2812_mask_clr  (1<<2)		// Bitmask to clear the data out pin
-
+  #define ws2812_mask_set  GPIO_PIN_9   // Bitmask to set the data out pin
+  #define ws2812_mask_clr  GPIO_PIN_9   // Bitmask to clear the data out pin
+#endif
 ///////////////////////////////////////////////////////////////////////
 // User defined area: Define CPU clock speed
 //
