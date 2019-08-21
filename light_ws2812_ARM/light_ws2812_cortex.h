a5a9ea43ee8897c18ae6b800d5cc608bb11912e7
Oliv4945 <oliv4945@gmail.com>
2019-08-21 09:08:35 +0200

    Symbol name unification

--- light_ws2812_ARM/light_ws2812_cortex.h
+++ light_ws2812_ARM/light_ws2812_cortex.h
@@ -35,19 +35,19 @@
 // mask and addresses may be the same.
 #ifdef LIGHT_WS2812_LPC
   // This example is for the NXP LPC81X
-  #define ws2812_port_set ((uint32_t*)&WS2812_GPIO_PORT->SET0)	// Address of the data port register to set the pin
-  #define ws2812_port_clr ((uint32_t*)&WS2812_GPIO_PORT->CLR0)	// Address of the data port register to clear the pin
+  #define ws2812_port_set ((uint32_t*)&LIGHT_WS2812_GPIO_PORT->SET0)	// Address of the data port register to set the pin
+  #define ws2812_port_clr ((uint32_t*)&LIGHT_WS2812_GPIO_PORT->CLR0)	// Address of the data port register to clear the pin
 
   #define ws2812_mask_set  (1<<2)		// Bitmask to set the data out pin
   #define ws2812_mask_clr  (1<<2)		// Bitmask to clear the data out pin
 #endif
 #ifdef LIGHT_WS2812_STM32
   // This example is for STM32 family
-  #define ws2812_port_set ((uint32_t*)&WS2812_GPIO_PORT->BSRR)  // Address of the data port register to set the pin
-  #define ws2812_port_clr ((uint32_t*)&WS2812_GPIO_PORT->BRR) // Address of the data port register to clear the pin
+  #define ws2812_port_set ((uint32_t*)&LIGHT_WS2812_GPIO_PORT->BSRR)  // Address of the data port register to set the pin
+  #define ws2812_port_clr ((uint32_t*)&LIGHT_WS2812_GPIO_PORT->BRR) // Address of the data port register to clear the pin
 
-  #define ws2812_mask_set  WS2812_GPIO_PIN   // Bitmask to set the data out pin
-  #define ws2812_mask_clr  WS2812_GPIO_PIN   // Bitmask to clear the data out pin
+  #define ws2812_mask_set  LIGHT_WS2812_GPIO_PIN   // Bitmask to set the data out pin
+  #define ws2812_mask_clr  LIGHT_WS2812_GPIO_PIN   // Bitmask to clear the data out pin
 #endif
 ///////////////////////////////////////////////////////////////////////
 // CPU clock speed
