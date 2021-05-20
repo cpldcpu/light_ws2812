a2bc285731a2e0eb96307fcc54073237179dbd59
Paul Wiegele <wiegele.paul@gmail.com>
2021-05-20 15:37:25 +0200

    added support for XMC4500

--- light_ws2812_ARM/light_ws2812_cortex.c
+++ light_ws2812_ARM/light_ws2812_cortex.c
@@ -44,7 +44,7 @@ void ws2812_sendarray(uint8_t *data,int datlen)
 			"		lsl %[dat],#24				\n\t"
 			"		movs %[ctr],#8				\n\t"
 			"ilop%=:							\n\t"
-			"		lsl %[dat], #1				\n\t"
+			"		lsls %[dat], #1				\n\t"
 			"		str %[maskhi], [%[set]]		\n\t"
 #if (w1&1)
 			ws2812_DEL1
@@ -79,7 +79,7 @@ void ws2812_sendarray(uint8_t *data,int datlen)
 #if (w2&16)
 			ws2812_DEL16
 #endif
-			"		sub %[ctr], #1				\n\t"
+			"		subs %[ctr], #1				\n\t"
 			"		str %[masklo], [%[clr]]		\n\t"
 			"		beq	end%=					\n\t"
 #if (w3&1)
