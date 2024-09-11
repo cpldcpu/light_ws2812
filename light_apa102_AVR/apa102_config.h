bea241c6f6f4ebf0c7ace3bd7fefdaa3c4117867
Matej Kocourek <matej@kocourek.cz>
2024-09-11 11:44:33 +0200

    Added packed attribute to structs that require it

--- light_apa102_AVR/apa102_config.h
+++ light_apa102_AVR/apa102_config.h
@@ -24,8 +24,8 @@
 // Define color byte order
 ///////////////////////////////////////////////////////////////////////
 
-struct cRGB { uint8_t b; uint8_t g; uint8_t r; };   // BGR (APA102 Standard)
-//struct cRGB { uint8_t g; uint8_t b; uint8_t r; };   // GBR 
+struct __attribute__ ((__packed__)) cRGB { uint8_t b; uint8_t g; uint8_t r; };   // BGR (APA102 Standard)
+//struct __attribute__ ((__packed__)) cRGB { uint8_t g; uint8_t b; uint8_t r; };   // GBR 
 
 
 
